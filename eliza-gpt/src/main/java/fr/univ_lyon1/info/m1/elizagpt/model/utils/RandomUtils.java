d1b3ff2a199906b848b79c6e731f2e4b0b4f8924
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 15:17:20 +0200

    Add javadoc

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/utils/RandomUtils.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/utils/RandomUtils.java
@@ -2,12 +2,30 @@ package fr.univ_lyon1.info.m1.elizagpt.model.utils;
 
 import java.util.Random;
 
-public class RandomUtils {
-    private static final Random random = new Random();
+/**
+ *  A class providing basics random methods.
+ */
+public final class RandomUtils {
+    private static final Random MY_RANDOM = new Random();
+    private RandomUtils() { }
+
+    /**
+     *  A fonction that return a random element from an array.
+     *
+     * @param array input array
+     * @param <T> type of the elements in the array
+     * @return a random element from the array
+     */
     public static <T> T pickArrayRandom(final T[] array) {
-        return array[random.nextInt(array.length)];
+        return array[MY_RANDOM.nextInt(array.length)];
     }
+
+    /**
+     * Fonction that acts as a coin toss.
+     *
+     * @return true or false randomly
+     */
     public static boolean coinToss() {
-        return random.nextBoolean();
+        return MY_RANDOM.nextBoolean();
     }
 }
