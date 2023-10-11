580343244d1ea3540d3490825a863dcfe4134b39
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 15:12:59 +0200

    Add javadoc

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/pattern/Pattern.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/pattern/Pattern.java
@@ -1,8 +1,17 @@
 package fr.univ_lyon1.info.m1.elizagpt.model.grammar.pattern;
 
+/**
+ *  Pattern class is used to describe regex pattern use to interpret user inputs.
+ */
 public class Pattern {
     private String regex;
-    public Pattern(String regex) {
+
+    /**
+     * Constructor by value that takes string parameter to build an instance.
+     *
+     * @param regex
+     */
+    public Pattern(final String regex) {
         this.regex = regex;
     }
 }
