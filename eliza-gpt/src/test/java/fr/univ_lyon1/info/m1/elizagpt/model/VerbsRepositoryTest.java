88458906c2c15e29fa298ee2c2eed837f2f28fad
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 10:13:51 +0200

    Testing if the file is correctly loaded

---
+++ eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/VerbsRepositoryTest.java
@@ -0,0 +1,29 @@
+package fr.univ_lyon1.info.m1.elizagpt.model;
+
+import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.*;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VerbsRepositoryTest {
+
+    @Test
+     void testVerbsRepository() {
+        try {
+
+            VerbsRepository verbsRepository = new VerbsRepository(Objects.requireNonNull(getClass().getResource("/vocabulary/vocabulary.xml")).toExternalForm());
+            List<Verb> loadedVerbs = verbsRepository.getVerbs();
+
+            assertNotNull(loadedVerbs);
+            assertFalse(loadedVerbs.isEmpty());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Une exception s'est produite lors du chargement des verbes. " + e.getMessage());
+        }
+    }
+}
