98ddd8319f8a1886f9c853f522ffda90bd9c3b72
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 15:21:34 +0200

    Build verbs repository from XML inputs file

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/verb/VerbsRepository.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/verb/VerbsRepository.java
@@ -1,4 +1,43 @@
 package fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that contain a list of verbs used by Eliza's response.
+ */
 public class VerbsRepository {
+    private final List<Verb> verbs;
+
+    /**
+     * List of 3rd group verbs and their correspondance from 1st person signular
+     * (Je) to 2nd person plural (Vous).
+     * Verbs are load from XML file
+     */
+    public VerbsRepository(final String xmlFilePath) throws IOException {
+        SAXBuilder saxBuilder = new SAXBuilder();
+        try {
+            Document document = saxBuilder.build(xmlFilePath);
+            Element rootElement = document.getRootElement();
+            List<Element> verbElements = rootElement.getChildren("verb");
+
+            verbs = new ArrayList<>();
+
+            for (Element verbElement : verbElements) {
+                String firstSingular = verbElement.getChildText("firstSingular");
+                String secondPlural = verbElement.getChildText("secondPlural");
+                verbs.add(new Verb(firstSingular, secondPlural));
+            }
+        } catch (Exception e) {
+            throw new IOException("Erreur lors du chargement du fichier XML : " + e.getMessage());
+        }
+    }
+    public List<Verb> getVerbs() {
+        return verbs;
+    }
 }
