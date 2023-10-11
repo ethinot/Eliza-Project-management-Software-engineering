ea9d1f8079c5002cf0efcbbaea21f3e7b84f5f46
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 15:24:23 +0200

    Initialise verb objet with first person of singular and second of the plural

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/verb/Verb.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/verb/Verb.java
@@ -1,4 +1,23 @@
 package fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb;
 
+/**
+ * Verb class that contain two strings representing the first person of the singular
+ * and the second of the plural.
+ */
 public class Verb {
+    private final String firstSingular;
+    private final String secondPlural;
+
+    Verb(final String firstSingular, final String secondPlural) {
+        this.firstSingular = firstSingular;
+        this.secondPlural = secondPlural;
+    }
+
+    public String getFirstSingular() {
+        return firstSingular;
+    }
+
+    public String getSecondPlural() {
+        return secondPlural;
+    }
 }
