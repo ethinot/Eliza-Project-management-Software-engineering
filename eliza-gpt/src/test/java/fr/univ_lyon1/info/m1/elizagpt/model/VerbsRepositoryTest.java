367276f75837b6e18bece4571ae238624667bbde
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 15:19:05 +0200

    Fix dependency importation

--- eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/VerbsRepositoryTest.java
+++ eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/VerbsRepositoryTest.java
@@ -1,12 +1,17 @@
 package fr.univ_lyon1.info.m1.elizagpt.model;
 
-import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.*;
+import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.VerbsRepository;
+import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.Verb;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 
 class VerbsRepositoryTest {
 
@@ -14,12 +19,12 @@ class VerbsRepositoryTest {
      void testVerbsRepository() {
         try {
 
-            VerbsRepository verbsRepository = new VerbsRepository(Objects.requireNonNull(getClass().getResource("/vocabulary/vocabulary.xml")).toExternalForm());
+            VerbsRepository verbsRepository = new VerbsRepository(Objects.requireNonNull(getClass()
+                    .getResource("/vocabulary/vocabulary.xml")).toExternalForm());
             List<Verb> loadedVerbs = verbsRepository.getVerbs();
 
             assertNotNull(loadedVerbs);
             assertFalse(loadedVerbs.isEmpty());
-
 
         } catch (IOException e) {
             e.printStackTrace();
