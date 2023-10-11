4171855e5ee18e3fef8452fca85f54621ee4e440
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-10-11 15:09:18 +0200

    Add javadoc

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/ElizaResponseProcessor.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/grammar/ElizaResponseProcessor.java
@@ -1,4 +1,20 @@
 package fr.univ_lyon1.info.m1.elizagpt.model.grammar;
 
+import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.VerbsRepository;
+import java.io.IOException;
+
+/**
+ * Main class for build and store Eliza responses.
+ */
 public class ElizaResponseProcessor {
+    private final VerbsRepository verbsRepository;
+
+    /**
+     * Build instances required by the class.
+     *
+     * @throws IOException exception throw if the file can't open.
+     */
+    public ElizaResponseProcessor() throws IOException {
+        verbsRepository = new VerbsRepository("./verb/vocabulary.xml");
+    }
 }
