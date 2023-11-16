f2cdd0b360ff6ef6e6c98d9e393e46bbca0b1854
Edouard Thinot <edouard.thinot@etu.univ-lyon1.fr>
2023-11-16 23:56:36 +0100

    Add correct and incorrect creation of VerbsRepository

--- eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/VerbsRepositoryTest.java
+++ eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/VerbsRepositoryTest.java
@@ -1,36 +1,39 @@
 package fr.univ_lyon1.info.m1.elizagpt.model;
 
-import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.VerbsRepository;
 import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.Verb;
+import fr.univ_lyon1.info.m1.elizagpt.model.grammar.verb.VerbsRepository;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
+
 import java.util.List;
-import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.fail;
-
-
+import static org.junit.jupiter.api.Assertions.*;
 
 class VerbsRepositoryTest {
 
     @Test
-     void testVerbsRepository() {
+    void testVerbsRepositoryContainsVerbs() {
         try {
+            VerbsRepository verbsRepository = new VerbsRepository("vocabulary/vocabulary.xml");
 
-            VerbsRepository verbsRepository = new VerbsRepository(Objects.requireNonNull(getClass()
-                    .getResource("/vocabulary/vocabulary.xml")).toExternalForm());
             List<Verb> loadedVerbs = verbsRepository.getVerbs();
 
-            assertNotNull(loadedVerbs);
-            assertFalse(loadedVerbs.isEmpty());
+            for (Verb verb : loadedVerbs) {
+                assertNotNull(verb.getFirstSingular());
+                assertNotNull(verb.getSecondPlural());
+            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail("Une exception s'est produite lors du chargement des verbes. " + e.getMessage());
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            fail("Une erreur s'est produite lors du chargement des verbes. " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testInvalidVerbsRepositoryCreation() {
+        try {
+            VerbsRepository invalidVerbsRepository = new VerbsRepository("ce/chemin/ne/mene/pas/a/rome");
+        } catch (Exception e) {
+            assertNotNull(e);
         }
     }
 }
