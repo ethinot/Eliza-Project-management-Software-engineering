88cec0a22126f25374272c8165e3568a459bbb24
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2023-11-26 15:51:00 +0100

    Improve research tests

--- eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/ResearchTest.java
+++ eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/ResearchTest.java
@@ -6,51 +6,65 @@ import fr.univ_lyon1.info.m1.elizagpt.model.messages.UserMessage;
 import fr.univ_lyon1.info.m1.elizagpt.model.researches.research_types.RegexpResearch;
 import fr.univ_lyon1.info.m1.elizagpt.model.researches.research_types.SubstringResearch;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Testing the research feature.
- *
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ResearchTest {
     private MessageRepository messageRepository;
+
     /**
      * Set up the testing MessageRepository.
-     *
      */
     @BeforeEach
     public void setUp() {
         messageRepository = new MessageRepository();
+        messageRepository.clear();
         messageRepository.sendMessage(new ElizaMessage("Bonjour, comment ça va?"));
         messageRepository.sendMessage(new UserMessage("Je vais bien, merci!"));
         messageRepository.sendMessage(new ElizaMessage("Quel temps fait-il aujourd'hui?"));
-        messageRepository.sendMessage(new
-                UserMessage("Il ne fait pas bien beau, j'ai envie de crever !"));
+        messageRepository.sendMessage(new UserMessage(
+                "Il ne fait pas beau, j'ai envie de crever!"));
     }
 
     /**
      * Testing the regexp research.
-     *
      */
     @Test
     public void testRegexpResearch() {
-        // Test de la recherche RegExp
         RegexpResearch regexpResearch = new RegexpResearch("Quel\\s+temps", messageRepository);
         regexpResearch.search("Quel\\s+temps", messageRepository);
 
-        // Vérifiez que le contenu du message correspond aux attentes
         assertEquals("Quel temps fait-il aujourd'hui?",
                 regexpResearch.getMessageRepositoryResult().get(0).getText());
-        // Le résultat devrait contenir deux messages correspondant au motif RegExp
         assertEquals(1, regexpResearch.getMessageRepositoryResult().size());
+
         regexpResearch.undoSearch();
         assertEquals(4, messageRepository.getAllMessages().size());
+    }
 
-        SubstringResearch substringResearch = new SubstringResearch("j'ai envie de crever",
+    /**
+     * Testing the substring research.
+     */
+    @Test
+    public void testSubstringResearch() {
+        SubstringResearch substringResearch = new SubstringResearch("j'ai ENVIE de creVer",
                 messageRepository);
-        substringResearch.search("j'ai envie de crever", messageRepository);
-        assertEquals(1, substringResearch.getMessageRepositoryResult().size());
+        substringResearch.search("j'ai ENVIE de creVer", messageRepository);
+
+        assertEquals("Il ne fait pas beau, j'ai envie de crever!",
+                substringResearch.getMessageRepositoryResult().get(0).getText());
+
+        substringResearch.undoSearch();
+        assertEquals(4, messageRepository.getAllMessages().size());
+
+        substringResearch.search("je ne match avec personne (snif)", messageRepository);
+        assertEquals(0, substringResearch.getMessageRepositoryResult().size());
     }
 }
